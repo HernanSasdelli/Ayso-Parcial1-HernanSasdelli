9b069c6fb59df2dc0e9239a4eb34649a7c13aeb1
HernanSasdelli <her9192copia@gmail.com>
2024-10-18 01:05:48 +0000

    P1-C1: Punto A: Estructuras simetricas

---
+++ puntoa.sh
@@ -0,0 +1,21 @@
+cat ~/.ssh/id_rsa.pub
+ssh -T git@github.com
+[200~git clone git@github.com:HernanSasdelli/Ayso-Parcial1-HernanSasdelli.git
+git clone git@github.com:HernanSasdelli/Ayso-Parcial1-HernanSasdelli.git
+mkdir -p Examenes-UTN/{alumno_1,alumno_2,alumno_3}/{parcial_1,parcial_2,parcial_3} Examenes-UTN/profesores
+ls
+mv Examenes-UTN ~/parcial1/
+ls
+cd parcial1
+ls
+cd Examenes-UTN/
+ls
+sudo apt update
+sudo apt install tree
+tree
+cd -
+cat > puntoa.sh
+tree
+history
+history | cut -c 8-
+history | cut -c 8->puntoa.sh
